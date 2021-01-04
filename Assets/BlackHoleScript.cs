dcfa974abef85eb65071171cf76b5252f2ef1417
ryo_nakano <naca.11868@gmail.com>
2021-01-04 16:40:57 +0900

    Ball Game

---
+++ Assets/BlackHoleScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BlackHoleScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider hit)
+    {
+        if (hit.gameObject.CompareTag("Player"))
+        {
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+            SceneManager.LoadScene(sceneIndex);
+        }
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
